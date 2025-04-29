b4e7992e4ae92985ea369fef56b94cba758cb180
sas0mir <sas_mir@icloud.com>
2025-04-29 17:59:25 +0200

    D_001: xml data parser

--- index.js
+++ index.js
@@ -2,31 +2,46 @@ const express = require('express');
 const app = express();
 const { PDFDocument } = require('pdf-lib');
 const fs = require('fs');
+const path = require('path');
+const { XMLParser } = require('fast-xml-parser');
+const _ = require('lodash');
 
 app.get('/', async (req, res) => {
     const page = await editPdf();
+    const data = await getNewData();
+    console.log('KEYS-->', _.get(data, ['yml_catalog', 'shop', 'offers', 'offer']));
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
   res.end(`<div>
     <h2>JS to PDF</h2>
-    <p>${JSON.stringify(page)}</p>
+    <p>${_.get(data, ['yml_catalog', 'shop', 'categories', 'category'])}</p>
+    <p>${_.get(data, ['yml_catalog', 'shop', 'offers', 'offer'])}</p>
     <div>`);
 });
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
 
+async function getNewData() {
+  const xmlParser = new XMLParser();
+  const xml = await fetch('https://rostmetall.ru/yandex.xml');
+  const text = await xml.text();
+  const data = await xmlParser.parse(text, true);
+  return data
+}
+
 async function editPdf() {
   // Load a PDFDocument from the existing PDF bytes
-  const existingPdfBytes = await fs.readFileSync('./katalog.pdf');
-
+  const pdfPath = path.join(__dirname, 'katalog.pdf');
+  const existingPdfBytes = await fs.readFileSync(pdfPath);
+  console.log('EXISTING PDF BYTES-->', typeof existingPdfBytes, existingPdfBytes.byteLength);
   const pdfDoc = await PDFDocument.load(existingPdfBytes);
 
   // Get the first page of the document
-  const pages = pdfDoc.getPages;
+  const pages = pdfDoc.getPages();
   const page = pages[6]
-    console.log('PAGE-->', pages.length, page);
+    console.log('PAGE-->', typeof page);
   return page
 
   // Draw a string of text diagonally across the first page
